cff4efe67514972453389bc928e76b72c4e06c27
azdren <azdreny@gmail.com>
2019-06-27 12:24:15 +0200

    book adding is done

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,8 @@
 const bookArray = []; // global array
 
-
+// MVP part
 /**
- * Adds two numbers together.
+ * This is the booj object
  * @param {string} title The title of the book
  * @param {string} author The author of the book
  * @param {int} numPages The number of pages of the book
@@ -15,7 +15,7 @@ function Book(title, author, numPages, isRead) {
   this.isRead = isRead;
 }
 /**
-*@param{Object} book is the newly created book
+*@param{Object} book adds a newly created book into array
 */
 function addBookToLibrary(book) {
   bookArray.push(book);
@@ -37,4 +37,38 @@ function readBook(index) {
       book.isRead = false;
     }
   });
+}
+// Listeners
+document.getElementById('submitBookBtn').addEventListener('click', createNewBook);
+
+/**
+* The handler of button 'Submit book'
+@param{e} e is the event
+*/
+function createNewBook(e) {
+  // getting data from the form
+  const title = document.getElementById('bookTitle').innerText;
+  const author = document.getElementById('bookAuthor').innerText;
+  const numPages = document.getElementById('bookNumPages').innerText;
+  const isRead = document.getElementById('isRead').checked? true : false;
+  // creating the Book object
+  const book = new Book(title, author, numPages, isRead);
+  // adding it to the main array of books
+  addBookToLibrary(book);
+  // removing test from the form
+  title.innerText = '';
+  author.innerText = '';
+  numPages.innerText = '';
+  // re-rendering the table of data
+  render();
+  console.log(title+' '+author+' '+numPages+' '+isRead);
+}
+
+// DOM Manipulation part
+
+/**
+* This method renders the books on the table
+*/
+function render() {
+
 }
